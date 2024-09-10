1f932d56982c1b471d2f46f026759ac6789bc81a
rougemeister <rougemeister1@gmail.com>
2024-09-10 15:39:18 +0000

    Added structure of tools cards

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,15 @@ import '../styles/main.scss';
 
 document.getElementById('content').innerHTML = `
   <ul >
-    ${tools.map(tool => `<li class="item">${tool.name} - ${tool.description}
-        <img src="./images/${tool.image}"/></li>`).join('')}
+    ${tools.map(tool => `<li class="item">
+        <div class='img-container'>
+            <img src="images/${tool.image}"/>
+        </div>
+        <div class='text'>
+        <h2>${tool.name}</h2>
+        <p>${tool.description}</p>
+        </div>
+        </li>`).join('')}
   </ul>
 `;
 
